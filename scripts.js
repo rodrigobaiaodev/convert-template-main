1eb9b4f87e3c96f9610d11d579539e7904c771af
rodrigobaiaodev <rodrigobaiao.dev@gmail.com>
2026-09-03 20:12:30 -0300

    Add initial HTML structure, styles, and JavaScript for currency converter
    - Created index.html with form for currency conversion
    - Added styles.css for layout and design
    - Implemented scripts.js for currency conversion logic
    - Included SVG images for logo and icons

---
+++ scripts.js
@@ -0,0 +1,50 @@
+//Cotação de moedas do dia.
+const USD = 4.87
+const EUR = 5.32
+const GBP = 6.08
+
+// Obtendo os elementos do formulário.
+const form = document.querySelector("form")
+const amount = document.getElementById("amount")
+const currency =document.getElementById("currency")
+const footer = document.querySelector("main footer")
+const description = document.getElementById("description")
+
+// Manipulando o input amount para receber somente números.
+amount.addEventListener("input", () => {
+  const hasCharactersRegex = /\D+/g
+  amount.value = amount.value.replace(hasCharactersRegex, "")
+})
+
+// Captando o evento de submit (enviar) do formulário.
+form.onsubmit = (event) => {
+  event.preventDefault()
+
+  switch (currency.value){
+    case "USD":
+      convertCurrency(amount.value, USD, "US$")
+      break
+    case "EUR":
+      convertCurrency(amount.value, EUR, "€")
+      break
+    case "GBP":
+      convertCurrency(amount.value, GBP, "£")
+      break
+  }
+}
+
+// Função para converter a moeda.
+function convertCurrency(amount, price, symbol){
+  try {
+    description.textContent = `${symbol} 1 = ${price}`
+
+    // Aplica a classe que exibe o footer para mostrar o resultado.
+    footer.classList.add("show-result")
+  } catch {
+    // Remove a classe do footer removendo ele da tela.
+    footer.classList.remove("show-result")
+    
+    console.log(error)
+    alert("Não foi possível converter. Tente novamente mais tarde.")
+  }
+}
